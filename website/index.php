67d51b639bcbd2a1820837c1bbad45d184b9fb14
backupApps <firmanadidayat11@gmail.com>
2024-12-19 10:29:04 +0700

    update footer

--- website/index.php
+++ website/index.php
@@ -279,8 +279,92 @@
       <!-- / Content -->
 
       <!-- Footer -->
-      <footer class="content-footer footer bg-footer-theme shadow" style="height: 25vh;">
-         <div class="container-xxl">
+      <footer class="content-footer footer bg-footer-theme shadow">
+         <div class="row container-xxl p-5 justify-content-center">
+            <div class="col-md-6 col-12">
+               <h5>About</h5>
+               <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tempore
+                  necessitatibus optio
+                  nostrum fugit nihil! Necessitatibus impedit, eos nostrum amet inventore sequi fugit, ducimus,
+                  provident iusto sint minima. Quia, nam?</p>
+            </div>
+            <div class="col-md-2 col-12">
+               <div class="card-header">
+                  <h5 class="mb-1">Connected Accounts</h5>
+               </div>
+               <div class="card-body">
+                  <div class="d-flex mb-4 align-items-center">
+                     <div class="flex-shrink-0">
+                        <img src="../assets/img/icons/brands/google.png" alt="google" class="me-4" height="32">
+                     </div>
+                     <div class="flex-grow-1 d-flex align-items-center justify-content-between">
+                        <div class="mb-sm-0 mb-2">
+                           <small>
+                              <h6 class="mb-0">Google</h6>
+                           </small>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="d-flex mb-4 align-items-center">
+                     <div class="flex-shrink-0">
+                        <img src="../assets/img/icons/brands/github.png" alt="github" class="me-4" height="32">
+                     </div>
+                     <div class="flex-grow-1 d-flex align-items-center justify-content-between">
+                        <div class="mb-sm-0 mb-2">
+                           <small>
+                              <h6 class="mb-0">Github</h6>
+                           </small>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-2 col-12">
+               <div class="card-header">
+                  <h5 class="mb-1">Social Accounts</h5>
+               </div>
+               <div class="card-body">
+                  <!-- Social Accounts -->
+                  <div class="d-flex mb-4 align-items-center">
+                     <div class="flex-shrink-0">
+                        <img src="../assets/img/icons/brands/facebook.png" alt="facebook" class="me-4" height="32">
+                     </div>
+                     <div class="flex-grow-1 row align-items-center">
+                        <div class="col-7">
+                           <a href="https://twitter.com/Theme_Selection" target="_blank" class="small">
+                              <h6 class="mb-0">Facebook</h6>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="d-flex mb-4 align-items-center">
+                     <div class="flex-shrink-0">
+                        <img src="../assets/img/icons/brands/twitter.png" alt="twitter" class="me-4" height="32">
+                     </div>
+                     <div class="flex-grow-1 row align-items-center">
+                        <div class="col-7">
+                           <a href="https://twitter.com/Theme_Selection" target="_blank" class="small">
+                              <h6 class="mb-0">Twitter</h6>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="d-flex mb-4 align-items-center">
+                     <div class="flex-shrink-0">
+                        <img src="../assets/img/icons/brands/instagram.png" alt="instagram" class="me-4" height="32">
+                     </div>
+                     <div class="flex-grow-1 row align-items-center">
+                        <div class="col-7">
+                           <a href="https://www.instagram.com/themeselection/" target="_blank" class="small">
+                              <h6 class="mb-0">Instagram</h6>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class=" container-xxl">
             <div
                class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
                <div class="text-body mb-2 mb-md-0">
@@ -303,7 +387,9 @@
                      target="_blank" class="footer-link">Support</a>
                </div>
             </div>
+
          </div>
+
       </footer>
       <!-- / Footer -->
 
