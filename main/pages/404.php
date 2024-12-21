4b5bd8605c03e120dc9ab5240cf4d7b5470ebca8
backupApps <firmanadidayat11@gmail.com>
2024-12-22 01:51:17 +0700

    fix 404 page

--- main/pages/404.php
+++ main/pages/404.php
@@ -1,6 +1,6 @@
 <!doctype html>
 
-<html lang="en" class="light-style layout-wide" dir="ltr" data-theme="theme-default" data-assets-path="assets/"
+<html lang="en" class="light-style layout-wide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
    data-template="vertical-menu-template-free" data-style="light">
 
 <head>
@@ -13,7 +13,7 @@
    <meta name="description" content="" />
 
    <!-- Favicon -->
-   <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+   <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
 
    <!-- Fonts -->
    <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -21,28 +21,28 @@
    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
       rel="stylesheet" />
 
-   <link rel="stylesheet" href="assets/vendor/fonts/remixicon/remixicon.css" />
+   <link rel="stylesheet" href="../assets/vendor/fonts/remixicon/remixicon.css" />
 
    <!-- Menu waves for no-customizer fix -->
-   <link rel="stylesheet" href="assets/vendor/libs/node-waves/node-waves.css" />
+   <link rel="stylesheet" href="../assets/vendor/libs/node-waves/node-waves.css" />
 
    <!-- Core CSS -->
-   <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
-   <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-   <link rel="stylesheet" href="assets/css/demo.css" />
+   <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+   <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+   <link rel="stylesheet" href="../assets/css/demo.css" />
 
    <!-- Vendors CSS -->
-   <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+   <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
    <!-- Page CSS -->
    <!-- Page -->
-   <link rel="stylesheet" href="assets/vendor/css/pages/page-misc.css" />
+   <link rel="stylesheet" href="../assets/vendor/css/pages/page-misc.css" />
 
    <!-- Helpers -->
-   <script src="assets/vendor/js/helpers.js"></script>
+   <script src="../assets/vendor/js/helpers.js"></script>
    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-   <script src="assets/js/config.js"></script>
+   <script src="../assets/js/config.js"></script>
 </head>
 
 <body>
@@ -54,13 +54,14 @@
       <h4 class="mb-2">Page Not Found 🙄</h4>
       <p class="mb-10 mx-2">we couldn't find the page you are looking for</p>
       <div class="d-flex justify-content-center mt-5">
-         <img src="assets/img/illustrations/tree-3.png" alt="misc-tree"
+         <img src="../assets/img/illustrations/tree-3.png" alt="misc-tree"
             class="img-fluid misc-object d-none d-lg-inline-block" />
-         <img src="assets/img/illustrations/misc-mask-light.png" alt="misc-error"
+         <img src="../assets/img/illustrations/misc-mask-light.png" alt="misc-error"
             class="misc-bg d-none d-lg-inline-block" height="172" data-app-light-img="illustrations/misc-mask-light.png"
             data-app-dark-img="illustrations/misc-mask-dark.png" />
          <div class="d-flex flex-column align-items-center">
-            <img src="assets/img/illustrations/404.png" alt="misc-error" class="misc-model img-fluid z-1" width="780" />
+            <img src="../assets/img/illustrations/404.png" alt="misc-error" class="misc-model img-fluid z-1"
+               width="780" />
             <div>
                <a href="?page=dashboard" class="btn btn-primary text-center my-6">Back to Dashboard</a>
             </div>
@@ -72,20 +73,20 @@
    <!-- / Content -->
 
    <!-- Core JS -->
-   <!-- build:js assets/vendor/js/core.js -->
-   <script src="assets/vendor/libs/jquery/jquery.js"></script>
-   <script src="assets/vendor/libs/popper/popper.js"></script>
-   <script src="assets/vendor/js/bootstrap.js"></script>
-   <script src="assets/vendor/libs/node-waves/node-waves.js"></script>
-   <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-   <script src="assets/vendor/js/menu.js"></script>
+   <!-- build:js ../assets/vendor/js/core.js -->
+   <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+   <script src="../assets/vendor/libs/popper/popper.js"></script>
+   <script src="../assets/vendor/js/bootstrap.js"></script>
+   <script src="../assets/vendor/libs/node-waves/node-waves.js"></script>
+   <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+   <script src="../assets/vendor/js/menu.js"></script>
 
    <!-- endbuild -->
 
    <!-- Vendors JS -->
 
    <!-- Main JS -->
-   <script src="assets/js/main.js"></script>
+   <script src="../assets/js/main.js"></script>
 
    <!-- Page JS -->
 
