94b89568f61e8c6035d8bfde623704034a9d0078
backupApps <firmanadidayat11@gmail.com>
2024-12-22 18:22:25 +0700

    edit transaction page

---
+++ main/pages/transaction/borrow/input.php
@@ -0,0 +1,71 @@
+<div class="container-xxl flex-grow-1 container-p-y">
+   <!-- Basic Layout -->
+   <div class="row">
+      <div class="col-xl">
+         <div class="card mb-6">
+            <div class="card-header d-flex justify-content-between align-items-center">
+               <h5 class="mb-0">Borrower
+            </div>
+            <div class="card-body">
+               <form>
+                  <div class="form-floating form-floating-outline mb-6">
+                     <input type="text" class="form-control" id="basic-default-fullname" placeholder="012.." />
+                     <label for="basic-default-fullname">NIK</label>
+                  </div>
+                  <div class="form-floating form-floating-outline mb-6">
+                     <input type="text" class="form-control" id="basic-default-company" placeholder="" />
+                     <label for="basic-default-company">Borrower's Name</label>
+                  </div>
+                  <div class="form-floating form-floating-outline mb-6">
+                     <input type="text" class="form-control" id="basic-default-company" placeholder="" />
+                     <label for="basic-default-company">Address</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+               </form>
+            </div>
+         </div>
+      </div>
+      <div class="col-xl">
+         <div class="card mb-6">
+            <div class="card-header d-flex justify-content-between align-items-center">
+               <h5 class="mb-0">
+                  The Book To Be Borrowed
+               </h5>
+            </div>
+            <div class="card-body">
+               <h6>Book 1</h6>
+               <form>
+                  <div class="input-group input-group-merge mb-6">
+                     <span class="input-group-text" id="basic-addon-search31"><i
+                           class="ri-search-line ri-20px"></i></span>
+                     <input type="text" class="form-control" placeholder="Code" aria-label="Search..."
+                        aria-describedby="basic-addon-search31" />
+                  </div>
+                  <div class="form-floating form-floating-outline mb-6">
+                     <input type="text" class="form-control" placeholder="Harry Potter; Games of Thrones; ..." />
+                     <label for="basic-default-company">Title</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+               </form>
+            </div>
+            <hr />
+            <div class="card-body">
+               <h6>Book 2</h6>
+               <form>
+                  <div class="input-group input-group-merge mb-6">
+                     <span class="input-group-text" id="basic-addon-search31"><i
+                           class="ri-search-line ri-20px"></i></span>
+                     <input type="text" class="form-control" placeholder="Code" aria-label="Search..."
+                        aria-describedby="basic-addon-search31" />
+                  </div>
+                  <div class="form-floating form-floating-outline mb-6">
+                     <input type="text" class="form-control" placeholder="Harry Potter; Games of Thrones; ..." />
+                     <label for="basic-default-company">Title</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
