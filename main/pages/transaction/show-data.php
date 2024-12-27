cd70c8b5de190d9d3a51b7d70e89236568f49f79
backupApps <firmanadidayat11@gmail.com>
2024-12-28 03:28:38 +0700

    make it 0 after return book

--- main/pages/transaction/show-data.php
+++ main/pages/transaction/show-data.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('process/read.php');
 $no = 1;
 ?>
@@ -7,15 +7,15 @@ $no = 1;
    <div class="card">
       <h5 class="card-header">Transaction | Borrower's Data</h5>
       <?php if (isset($_SESSION['msg']['return'])) { ?>
-      <div class="alert alert-success ms-2 me-2" role="alert">
-         <?php echo $_SESSION['msg']['return'];?>
-      </div>
+         <div class="alert alert-success ms-2 me-2" role="alert">
+            <?php echo $_SESSION['msg']['return']; ?>
+         </div>
       <?php } ?>
 
       <?php if (isset($_SESSION['msg']['delete'])) { ?>
-      <div class="alert alert-success ms-2 me-2" role="alert">
-         <?php echo $_SESSION['msg']['delete'];?>
-      </div>
+         <div class="alert alert-success ms-2 me-2" role="alert">
+            <?php echo $_SESSION['msg']['delete']; ?>
+         </div>
       <?php } ?>
       <div class="table-responsive text-nowrap">
          <table class="table table-striped">
@@ -31,31 +31,31 @@ $no = 1;
                </tr>
             </thead>
             <tbody class="table-border-bottom-0">
-               <?php while($data = mysqli_fetch_array($query)) { ?>
-               <tr>
-                  <td><?php echo $no++; ?></td>
-                  <td><?php echo $data['nik']; ?></td>
-                  <td><?php echo $data['name'] ?></td>
-                  <td><?php echo $data['borrowed_books'] ?>/5</td>
-                  <td><?php echo $data['borrow_date'] ?></td>
-                  <td><?php echo ($data['return_date'] != null) ? $data['return_date'] : '<b>Not return yet</b>' ?></td>
-                  <td>
-                     <a href="" class="btn btn-sm btn-info">
-                        Edit
-                        <i class="ri-pencil-line"></i>
-                     </a> |
-                     <a href="?page=transaction/detail-borrower&id=<?php echo $data['id_transaksi']; ?>"
-                        class="btn btn-sm btn-warning">
-                        Detail
-                        <i class="ri-book-open-line"></i>
-                     </a> |
-                     <a href="pages/transaction/process/delete.php?nik=<?php echo $data['nik']; ?>"
-                        onclick="return confirm('Anda yakin ingin menghapus data ini?')" class="btn btn-sm btn-danger">
-                        <i class="ri-delete-bin-line"></i>
-                        Delete
-                     </a>
-                  </td>
-               </tr>
+               <?php while ($data = mysqli_fetch_array($query)) { ?>
+                  <tr>
+                     <td><?php echo $no++; ?></td>
+                     <td><?php echo $data['nik']; ?></td>
+                     <td><?php echo $data['name'] ?></td>
+                     <td><?php echo ($data['return_date'] != null) ? '0' : $data['borrowed_books'] ?>/5</td>
+                     <td><?php echo $data['borrow_date'] ?></td>
+                     <td><?php echo ($data['return_date'] != null) ? $data['return_date'] : '<b>Not return yet</b>' ?></td>
+                     <td>
+                        <a href="" class="btn btn-sm btn-info">
+                           Edit
+                           <i class="ri-pencil-line"></i>
+                        </a> |
+                        <a href="?page=transaction/detail-borrower&id=<?php echo $data['id_transaksi']; ?>"
+                           class="btn btn-sm btn-warning">
+                           Detail
+                           <i class="ri-book-open-line"></i>
+                        </a> |
+                        <a href="pages/transaction/process/delete.php?nik=<?php echo $data['nik']; ?>"
+                           onclick="return confirm('Anda yakin ingin menghapus data ini?')" class="btn btn-sm btn-danger">
+                           <i class="ri-delete-bin-line"></i>
+                           Delete
+                        </a>
+                     </td>
+                  </tr>
                <?php } ?>
             </tbody>
          </table>
