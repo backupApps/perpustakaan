863d7a7e68eb1760f320a4983a3ba78d79670704
backupApps <firmanadidayat11@gmail.com>
2024-12-28 06:10:44 +0700

    change message

--- main/pages/transaction/process/search.php
+++ main/pages/transaction/process/search.php
@@ -5,35 +5,34 @@ include('../../../../components/connection.php');
 
 // Mengecek apakah parameter 'q' ada
 if (isset($_GET['q'])) {
-   $nik = $_GET['q'];
+    $nik = $_GET['q'];
 
-   // Query untuk mencari nama berdasarkan NIK
-   $sql = "SELECT name FROM member WHERE nik = '$nik' LIMIT 1";
-   $query = mysqli_query($connect, $sql);
+    // Query untuk mencari nama berdasarkan NIK
+    $sql = "SELECT name FROM member WHERE nik = '$nik' LIMIT 1";
+    $query = mysqli_query($connect, $sql);
 
-   if (mysqli_num_rows($query) > 0) {
-       // Menampilkan nama jika NIK ditemukan
-       $data = mysqli_fetch_array($query);
-       echo $data['name'];
-   } else {
-       // Menampilkan pesan jika NIK tidak ditemukan
-       echo "No member found with this NIK!";
-   }
+    if (mysqli_num_rows($query) > 0) {
+        // Menampilkan nama jika NIK ditemukan
+        $data = mysqli_fetch_array($query);
+        echo $data['name'];
+    } else {
+        // Menampilkan pesan jika NIK tidak ditemukan
+        echo "Tidak ada data anggota berdasarkan NIK ini!";
+    }
 }
 
 if (isset($_GET['b'])) {
     $code = $_GET['b'];
- 
+
     $sql = "SELECT title FROM book WHERE code = '$code' LIMIT 1";
     $query = mysqli_query($connect, $sql);
- 
+
     if (mysqli_num_rows($query) > 0) {
         $data = mysqli_fetch_array($query);
         echo $data['title'];
     } else {
-        echo "No book found with this code!";
+        echo "Tidak ada data buku berdasarkan kode ini!";
     }
- }
+}
 
 mysqli_close($connect);
-?>
