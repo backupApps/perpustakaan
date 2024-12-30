404d7980c6743842980f8c3be3c738275f3ccee5
backupApps <firmanadidayat11@gmail.com>
2024-12-31 04:46:43 +0700

    ascending data

--- main/pages/book/process/read.php
+++ main/pages/book/process/read.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 include('../components/connection.php');
 $sql = "SELECT * FROM book
          LEFT JOIN category ON book.category_code = category.category_code 
-         LEFT JOIN publisher ON book.publisher_code = publisher.publisher_code";
+         LEFT JOIN publisher ON book.publisher_code = publisher.publisher_code
+         ORDER BY book.title ASC";
 
 $query = mysqli_query($connect, $sql);
 
