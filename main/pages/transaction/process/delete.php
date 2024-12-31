7d673270ea5d2d72a9f1f47e121ab7ae14ba3113
= <=>
2025-01-01 01:30:38 +0700

    change target delete

--- main/pages/transaction/process/delete.php
+++ main/pages/transaction/process/delete.php
@@ -1,11 +1,15 @@
 <?php
 
-$nik = $_REQUEST['nik'];
+$id = $_REQUEST['id'];
 
 include('../../../../components/connection.php');
-$sql = "DELETE FROM transaksi WHERE nik_member='$nik'";
+$sql = "DELETE FROM transaksi WHERE id='$id'";
 $query = mysqli_query($connect, $sql);
 
+$sql = "SELECT nik_member FROM transaksi";
+$query = mysqli_query($connect, $sql);
+$data = mysqli_fetch_array($query);
+
 session_start();
-$_SESSION['msg']['delete'] = "Data peminjam <b>'". $nik ."</b>' berhasil di hapus!";
+$_SESSION['msg']['delete'] = "Data peminjam <b>'" . $data['nik_member'] . "'</b> berhasil di hapus!";
 header('location: ../../../?page=transaction/show-data');
