81c7f307f5458605862f229befb52b99a9b69c16
= <=>
2025-01-04 01:34:09 +0700

    added validation

--- main/pages/member/process/create.php
+++ main/pages/member/process/create.php
@@ -26,10 +26,6 @@ $ekstensiGambar = end($ekstensiGambar);
 // fungsi waktu
 $photo = date('l, d-m-Y  H:i:s');
 
-// generate nama baru
-$newName = strtolower(md5($photo).'.'.$ekstensiGambar);
-$upload = move_uploaded_file($fileTmp, $folder.$newName);
-
 // value
 $_SESSION['value']['nik'] = $nik;
 $_SESSION['value']['name'] = $name;
@@ -41,27 +37,45 @@ $_SESSION['value']['photo'] = $photo;
 // validation
 if ($nik == '') {
    $_SESSION['msg']['nik'] = "Kolom NIK tidak boleh kosong!";
+} else if (strlen($nik) < 16 || strlen($nik) > 16) {
+   $_SESSION['msg']['nik'] = "NIK harus 16 karakter!";
+} else if (!ctype_digit($nik)) { // Validasi hanya angka
+   $_SESSION['msg']['nik'] = "NIK hanya boleh berisi angka!";
 }
 if ($name == '') {
    $_SESSION['msg']['name'] = "Kolom nama tidak boleh kosong!";
+} else if (preg_match('/\d/', $name)) { // Validasi tidak boleh ada angka
+   $_SESSION['msg']['name'] = "Nama tidak boleh mengandung angka!";
 }
 if ($phoneNumber == '') {
    $_SESSION['msg']['phoneNumber'] = "Kolom nomor telepon tidak boleh kosong!";
+} else if (!ctype_digit($phoneNumber)) { // Validasi hanya angka
+   $_SESSION['msg']['phoneNumber'] = "Nomor telepon hanya boleh berisi angka!";
 }
-if ($email == '') {
-   $_SESSION['msg']['email'] = "Kolom email tidak boleh kosong!";
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+   $_SESSION['msg']['email'] = "Format email tidak valid!";
 }
 if ($address == '') {
    $_SESSION['msg']['address'] = "Kolom address tidak boleh kosong!";
 }
-if ($photo == '') {
+if (empty($photo)) {
    $_SESSION['msg']['photo'] = "Pilih Gambar!";
-} else if (!$upload) {
-   $_SESSION['msg']['photo'] = "Gagal meng-upload file.";
-   header('location: ../../../?page=member/input-member');
-   exit();
-}
+} else if (!in_array($ekstensiFile, $ekstensiValid)) { // Validasi ekstensi file
+   $_SESSION['msg']['photo'] = "Hanya file dengan ekstensi jpg, jpeg, atau png yang diperbolehkan!";
+} else if ($_FILES['photo']['size'] > 2 * 1024 * 1024) { // Validasi ukuran file maksimal 2MB
+   $_SESSION['msg']['photo'] = "Ukuran file maksimal 2MB!";
+} else {
+   // Jika validasi berhasil, upload file
+   // generate nama baru
+   $newName = strtolower(md5($photo) . '.' . $ekstensiGambar);
+   $upload = move_uploaded_file($fileTmp, $folder . $newName);
 
+   if (!$upload) {
+      $_SESSION['msg']['photo'] = "Gagal meng-upload file.";
+      header('location: ../../../?page=member/input-member');
+      exit();
+   }
+}
 if (isset($_SESSION['msg'])) {
    header('location: ../../../?page=member/input-member');
    exit();
